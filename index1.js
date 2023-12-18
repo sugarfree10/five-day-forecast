c6fdebc31f4cd3349a28ab0bd9e33d80b82e32ba
sugarfree10 <2004sugarfree@gmail.com>
2023-12-18 14:42:34 -0800

    longitude and latitude

--- index1.js
+++ index1.js
@@ -1,12 +1,17 @@
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            const coords = position.coords;
-            const currentLat = position.latitude;
-            const currentLong = position.longitude;
-            console.log(currentLat, currentLong);
-      });
-    } else {
-       console.log("Geolocation is not supported by this browser.");
-};
+
+const apiKey = "93e2ae367c4f1218ca8ee3a345186cd5";
   
+
+
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const coords = position.coords;
+        const currentLat = coords.latitude;
+        const currentLong = coords.longitude;
+        console.log(currentLat, currentLong);
+    });
+} else {
+    console.log("Geolocation is not supported by this browser.");
+}
