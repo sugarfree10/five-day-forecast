8e5c84b9fa569f6ad606656dfe6f0b30bb874944
sugarfree10 <2004sugarfree@gmail.com>
2023-12-21 04:32:38 -0800

    the main js part finished

--- index1.js
+++ index1.js
@@ -5,6 +5,9 @@ import { showTodayForecast, showFiveDaysForecast  } from "./index2.js";
 
 const apiKey = "93e2ae367c4f1218ca8ee3a345186cd5";
 
+const searchBar = document.getElementById("search-btn");
+const searchInput = document.getElementById("search-input");
+
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
         const coords = position.coords;
@@ -52,6 +55,47 @@ const fetchWeatherData = async (lat, long) => {
     
       
 };
+
+// searchBar.addEventListener("click", async () => {
+//     const inputValue = searchInput.value.trim();
+//     if (inputValue === "") {
+//         Swal.fire("Please, Enter Something!");
+//     } else {
+//         const response = await fetch(
+//             `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&appid={apiKey}`
+          
+//         );
+//         console.log(response);
+//         searchInput.value = "";
+//     }
+    
+// });
+searchBar.addEventListener("click", async () => {
+    const inputValue = searchInput.value.trim();
+    if (inputValue === " ") {
+        Swal.fire("Please, Enter Something!");
+    } else {
+        try {
+            const response = await fetch(
+                `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&appid=${apiKey}`
+            )
+
+            const data = await response.json();
+
+            const locationCoords = data[0];
+            const lat = locationCoords.lat;
+            const lon = locationCoords.lon;
+
+            fetchWeatherData(lat, lon);
+            searchInput.value = "";
+        } catch (error) {
+            Swal.fire("Please, Enter Valid City!"); 
+        }
+    }
+});
+
+
+
 
 
 
