e442fa89359ff94052ed7fdad7ed119d9d907fa9
sugarfree10 <2004sugarfree@gmail.com>
2023-12-24 04:07:58 -0800

    creating images for cards and today weather

--- index2.js
+++ index2.js
@@ -16,11 +16,26 @@ const showTodayForecast = (weatherData, city) => {
     const currentLocation = document.getElementById("today-location");
     const currentDate = document.getElementById("todayDate-container");
     const todayClouds = document.getElementById("today-clouds");
+    const todayCloudsImage = document.getElementById("today-cloud-image");
     const windHighlight = document.getElementById("wind-highlight");
     const humidityHighlight = document.getElementById("humidity-highlight");
     const vision = document.getElementById("vision");
     const airHigh = document.getElementById("air-highlight");
     const todayTemp = document.getElementById("today-temp");
+    
+    if (weatherData.weather[0].main === "Clouds") {
+        todayCloudsImage.innerHTML = `
+        <img src="./images/LightCloud.png" alt="picture">
+        `;
+    } else if (weatherData.weather[0].main === "Clear") {
+        todayCloudsImage.innerHTML = `
+        <img src="./images/Clear.png" alt="picture">
+        `;
+    } else if (weatherData.weather[0].main === "Rain") {
+        todayCloudsImage.innerHTML = `
+        <img src="./images/Shower.png">
+        `
+    }
 
     todayTemp.innerHTML = `
     <h6> ${fahrenheitToCelsius(weatherData.main.temp).toFixed(1)} ℃</h6>
@@ -38,6 +53,8 @@ const showTodayForecast = (weatherData, city) => {
     todayClouds.innerHTML = `
     <h3> ${weatherData.weather[0].main} </h3>
     `;
+
+ 
      
     windHighlight.innerHTML = `
     <h4> ${weatherData.wind.speed} mph </h4>
@@ -54,6 +71,7 @@ const showTodayForecast = (weatherData, city) => {
     airHigh.innerHTML = `
     <h4> ${weatherData.main.pressure} mb </h4>
     `;
+    
 
     
 };
@@ -61,15 +79,28 @@ const showTodayForecast = (weatherData, city) => {
 const showFiveDaysForecast = (pairedWeatherInfo) => {
     const fiveDaysCards = document.getElementById("five-cards");
     const cardsDate = document.getElementById("card-date");
+    
+
+    cardsDate.innerHTML = "";
 
     for (const key in pairedWeatherInfo) {
         const item = pairedWeatherInfo[key][0];
-        
+        console.log(item);
+        let imageSrc = "";
+        if (item.weather[0].main === "Clouds") {
+            imageSrc = "./images/LightCloud.png";
+        } else if (item.weather[0].main === "Clear") {
+            imageSrc = "./images/Clear.png";
+        } else if (item.weather[0].main === "Rain") {
+            imageSrc = "./images/Shower.png";
+        }
+
         cardsDate.innerHTML += `
         <div style = "width: 15%; height: 170px; background-color: #1E213A; display: flex; flex-direction: column; justify-content: space-evenly;">
             <p style = "front-size: 3px;"> ${new Date(item.dt_txt).toUTCString().replace(' GMT', '')} </p>
-            
+            <img src="${imageSrc}" alt="${item.weather[0].main}">
             <h5 class="temperature"> ${fahrenheitToCelsius(item.main.temp).toFixed(1)} ℃</h5>
+            
         </div>
         `
     }
